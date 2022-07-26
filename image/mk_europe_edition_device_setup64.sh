1819083acc6e03ac298c3d8d8676f5434089b2d1
Patrick Timoney <patrick.timoney@gmail.com>
2022-07-26 15:47:55 +0100

    remove mounting sda1

--- image/mk_europe_edition_device_setup64.sh
+++ image/mk_europe_edition_device_setup64.sh
@@ -175,8 +175,8 @@ mkdir -p /overlay/robase # prepare so we can bind-mount root even if overlay is 
 #startup scripts
 cp -f rc.local /etc/rc.local
 
-# Optionally mount /dev/sda1 as /var/log - for logging to USB stick
-echo -e "\n/dev/sda1             /var/log        auto    defaults,nofail,noatime,x-systemd.device-timeout=1ms  0       2" >> /etc/fstab
+# # Optionally mount /dev/sda1 as /var/log - for logging to USB stick
+# echo -e "\n/dev/sda1             /var/log        auto    defaults,nofail,noatime,x-systemd.device-timeout=1ms  0       2" >> /etc/fstab
 
 #disable serial console, disable rfkill state restore, enable wifi on boot
 sed -i /boot/cmdline.txt -e "s/console=serial0,[0-9]\+ /systemd.restore_state=0 rfkill.default_state=1 /"
